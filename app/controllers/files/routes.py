c53790f435e80357d13b05d60878ed2a503adcad
laurensvm <laurens@vanmieghem.org>
2019-11-01 14:10:36 +1100

    Revert "File routes"
    This reverts commit e994b5f3e04c8881aa81085b52986503bb284f2e.

--- app/controllers/files/routes.py
+++ app/controllers/files/routes.py
@@ -5,24 +5,8 @@ from ..authentication import auth
 from ..statuscodes import bad_request, not_found, unauthorized
 from ...models import File, Directory
 
-
 @auth.login_required
 @files.route("/", methods=["POST"])
-def get_file_by_path():
-    path = request.json.get("path")
-
-    f = File.query.filter_by(path=path).first()
-    if not f:
-        return not_found("File with path {0} is not found".format(path))
-
-    if not g.current_user in f.directory.users_with_rights:
-        return unauthorized()
-
-    return jsonify(f.json())
-
-
-@auth.login_required
-@files.route("/files/", methods=["POST"])
 def get_files_in_directory():
     path = request.json.get("path") or request.json.get("directory")
     if not path:
@@ -36,8 +20,7 @@ def get_files_in_directory():
     if not g.current_user in d.users_with_rights:
         return unauthorized()
 
-    return jsonify({'files': [ file.json() for file in d.files ]})
-
+    return jsonify({'files': [file.json() for file in d.files ]})
 
 @auth.login_required
 @files.route("/<int:id/", methods=["GET"])
@@ -51,16 +34,3 @@ def get_file_by_id(id):
         return unauthorized()
 
     return jsonify(f.json())
-
-
-@auth.login_required
-@files.route("/<string:name>/", methods=["GET"])
-def get_files_with_name(name):
-    files = File.query.filter_by(name=name)
-
-    files_with_rights = []
-    for file in files:
-        if g.current_user in file.directory.users_with_rights:
-            files_with_rights.append(file)
-
-    return jsonify({'files': [ f.json() for f in files_with_rights ]})
